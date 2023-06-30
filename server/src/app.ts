f05a86469921f9c52fc4a88882f9f087a039ba57
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-06-30 19:10:04 +0300

    Handle basic GET request

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,20 @@
+import fs from 'fs';
 import express from 'express';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+
+export interface Book {
+  id: number;
+  isbn: number;
+  title: string;
+  subtitle: string;
+  author: string;
+  published: Date;
+  publisher: string;
+  pages: number;
+  description: string;
+  website: string;
+}
 
 dotenv.config();
 
@@ -11,8 +25,18 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+const books: Book[] = JSON.parse(
+  fs.readFileSync('./dev-data/data/books.json', 'utf8')
+);
+
+app.get('/api/v1/books', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: books.length,
+    data: {
+      books,
+    },
+  });
 });
 
 app.listen(PORT, () => {
