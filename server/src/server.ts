7c0729ec1ad2a4d3f938a9b163c84299d3b0ef2c
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-07-02 18:35:10 +0300

    Setup MongoDB, Mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,21 @@
+import mongoose from 'mongoose';
 import 'dotenv/config';
 import app from './app';
+
+const { MONGODB_URI } = process.env;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env'
+  );
+}
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('DB connection successful! 🟢'));
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} ⚡...`);
+  console.log(`Server running on port ${PORT} 🚀...`);
 });
