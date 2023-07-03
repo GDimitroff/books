be40cbb2bb7aea74537f54fb6521746203b7130f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-07-03 19:46:06 +0300

    Create simple Book model

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import app from './app';
+import { Book } from './controllers/bookController';
 
 const { MONGODB_URI } = process.env;
 
@@ -13,6 +14,24 @@ if (!MONGODB_URI) {
 mongoose
   .connect(MONGODB_URI)
   .then(() => console.log('DB connection successful! 🟢'));
+
+const bookSchema = new mongoose.Schema<Book>({
+  title: {
+    type: String,
+    required: [true, 'A book must have a title'],
+    unique: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  pages: {
+    type: Number,
+    required: [true, 'A book must have a number of pages'],
+  },
+});
+
+const Book = mongoose.model<Book>('Book', bookSchema);
 
 const PORT = process.env.PORT || 8000;
 
